23d4e790b99f6d82e36c987aa9aac8cb1f5b1eaa
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-04-19 21:59:24 +1000

    Testing having stuff in the porject
    this is a test to see if i can commit the project with things in the project

---
+++ FeungShuiUnity/Assets/Scripts/SmallLife.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallLife : MonoBehaviour
+{
+    private float life;
+    void Start()
+    {
+        life = Time.time;
+    }
+
+
+    void Update()
+    {
+        if (Time.time - life >= 2) {
+            Destroy(gameObject);
+        }
+    }
+}
