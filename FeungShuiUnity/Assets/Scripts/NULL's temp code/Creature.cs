eb502eb6978bf9efd3798b5c83f5ea64eb1b1568
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-04-21 22:26:12 +1000

    changed some things

--- FeungShuiUnity/Assets/Scripts/NULL's temp code/Creature.cs
+++ FeungShuiUnity/Assets/Scripts/NULL's temp code/Creature.cs
@@ -8,19 +8,33 @@ public class Creature : MonoBehaviour
     //so this class holds all the essential things that every spirit has in common (functionality wise)
 
     //stats
-    public float activeHealth;
-    public float criticalHealth;
-    public float attack;
-    public float defense;
-    public float spAttack; // i see Mega had intelligence (im guessing)
-    public float spDefense; // Mega had resistance for the name of this stat
-    public float speed;
-    public Types_2 Type;
+    private float activeHealth;
+    private float criticalHealth;
+    private float attack;
+    private float defense;
+    private float spAttack; // i see Mega had intelligence (im guessing)
+    private float spDefense; // Mega had resistance for the name of this stat
+    private float speed;
+    private string type;
 
-    public void takeDamage (Creature other) {
-        //call the getEffectiveness function and see if the type matches the current spirits to get the effectiveness multiplier
+    //Type effectiveness dictionaries
+    private static Dictionary<string,List<string>> strongTypeEffectiveness = new Dictionary<string,List<string>>{
+        {"Fire", new List<string>{"Plant","Beast"}},
+        {"Water", new List<string>{"Fire","Earth","Stone"}},
+        {"Earth", new List<string>{"Fire","Electricity"}},
+        {"Wind", new List<string>{"Plant"}}
+        //add more
+    }; //this is for types that the key type DEALS extra damage TO
+    private static Dictionary<string,List<string>> weakTypeEffectiveness = new Dictionary<string,List<string>>{
+        {"Fire", new List<string>{"Earth", "Water"}},
+        {"Water", new List<string>{"Plant", "Ice"}},
+        {"Earth", new List<string>{"Water", "Ice"}},
+        {"Wind", new List<string>{"Electricity", "Stone"}}
+        //add more
+    }; //this is for types that the key type TAKES extra damage FROM
+    
+
+    public string getType() {
+        return this.type;
     }
-
-    // etc.. i have to go to sleep now (1:41 am AEST)
-
 }
--- FeungShuiUnity/Assets/Scripts/NULL's temp code/Types_2.cs
+++ FeungShuiUnity/Assets/Scripts/NULL's temp code/Types_2.cs
@@ -5,30 +5,15 @@ using UnityEngine;
 public class Types_2
 {
     // holds the types and rudimentary type effectiveness in a dictionary
-    static Dictionary<string,List<string>> strongTypeEffectiveness = new Dictionary<string,List<string>>{
-        {"Fire", new List<string>{"Plant","Beast"}},
-        {"Water", new List<string>{"Fire","Earth","Stone"}},
-        {"Earth", new List<string>{"Fire","Electricity"}},
-        {"Wind", new List<string>{"Plant"}}
-    }; //this is for types that the key type DEALS extra damage TO
-
-    static Dictionary<string,List<string>> weakTypeEffectiveness = new Dictionary<string,List<string>>{
-        {"Fire", new List<string>{"Earth", "Water"}},
-        {"Water", new List<string>{"Plant", "Ice"}},
-        {"Earth", new List<string>{"Water", "Ice"}},
-        {"Wind", new List<string>{"Electricity", "Stone"}}
-    }; //this is for types that the key type TAKES extra damage FROM
-
-
     //dunno if you guys want to change this (probably) 
     //could just use a text file
 
-    public static Dictionary<string,List<string>> getEffectiveness (bool Strong) {
+    /*public static Dictionary<string,List<string>> getEffectiveness (bool Strong) {
         if (Strong) { // its true
             return strongTypeEffectiveness;
         } else {
             return weakTypeEffectiveness;
         }
     }
-
+*/
 }
