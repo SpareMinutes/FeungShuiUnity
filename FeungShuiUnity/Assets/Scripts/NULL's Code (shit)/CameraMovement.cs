368a0374c6c645ff39818e088791f95ec19683d3
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-04-26 00:12:34 +1000

    camera movement
    added camera movement so that camera moves with the player

---
+++ FeungShuiUnity/Assets/Scripts/NULL's Code (shit)/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject toFollow;
+
+    void Update () {
+        gameObject.transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, -10);
+    }
+}
