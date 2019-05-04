bc0db7b4322bf25996820564f752356b74a15a92
jumpingbeanz <jumpingpbeans@gmail.com>
2019-05-05 01:26:15 +0800

    Did some stuff on Battle Unity
    Have no clue what im doing half the time :/

---
+++ FeungShuiUnity/Assets/Scripts/Jumping's Bad Code/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_game_handler : MonoBehaviour
+{
+    private GameObject OverallHP;
+   
+    [SerializeField] private OverallHP healthbar;
+    
+    private void Start()
+    {
+        healthbar.SetSize(.4f);
+    }
+
+    // Update is called once per frame
+
+}
---
+++ FeungShuiUnity/Assets/Scripts/Jumping's Bad Code/HP_Bar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HP_Bar : MonoBehaviour
+{
+    // shows HP bar at 40 percent HP
+    void Start()
+    {
+        Transform bar = transform.Find("Bar");
+        bar.localScale = new Vector3(.4f, 1f);
+    }
+
+
+
+
+
+
+    //for when I (Jumpingbeans) know how to use unity
+    /*private Transform bar;
+
+    void Start()
+    {
+        bar = transform.Find("Bar");
+
+    }
+
+    // Update is called once per frame
+    
+    public void SetSize(float sizeNormalized){
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }*/
+}
