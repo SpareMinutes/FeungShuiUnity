4d344491192e3d85c4690257af891a100a070c7f
MegaMinerd <megaminerd@gmail.com>
2019-05-30 15:14:56 -0400

    Redid movement calculations

--- FeungShuiUnity/Assets/Scripts/NULL's Code/TestWalking.cs
+++ FeungShuiUnity/Assets/Scripts/NULL's Code/TestWalking.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestWalking : MonoBehaviour
-{
-    public float speed; // movement speed multiplier
-    public Animator animator;
+public class TestWalking : MonoBehaviour{
+    [SerializeField]
+    private float speed; //movement speed multiplier
+    [SerializeField]
+    private Animator animator;
 
     private float HorizontalDirection;
     private float VerticalDirection;
@@ -25,31 +26,32 @@ public class TestWalking : MonoBehaviour
 
     private void OmnidirectionalMovement() {
         //get inputs (controller or keyboard)
-        HorizontalDirection = Input.GetAxis("Horizontal")*Mathf.Abs(Input.GetAxisRaw("Horizontal")); //extra bit for less floatiness
-        VerticalDirection = Input.GetAxis("Vertical")*Mathf.Abs(Input.GetAxisRaw("Vertical")); //extra bit for less floatiness
+        HorizontalDirection = Input.GetAxis("Horizontal");
+        VerticalDirection = Input.GetAxis("Vertical");
 
-        // seting up the animator settings
-        animator.SetBool("isWalking", HorizontalDirection != 0 || VerticalDirection != 0); 
-        animator.SetFloat("HorizontalSpeed", HorizontalDirection);
-        animator.SetFloat("VerticalSpeed", VerticalDirection);
-
-        //angle is to account for speed difference in the angles of movement not parallel to the XY plane
-        float angle;
-        if (HorizontalDirection == 0){
-            angle = 90;
-        } else {
-            angle = Mathf.Abs(Mathf.Atan(VerticalDirection/HorizontalDirection));
+        //normalize keyboard inputs
+        float hypotenuse = Mathf.Sqrt(Mathf.Pow(HorizontalDirection, 2)+ Mathf.Pow(VerticalDirection, 2));
+        if (hypotenuse > 1){
+            HorizontalDirection /= hypotenuse;
+            VerticalDirection /= hypotenuse;
         }
 
-        float newX = HorizontalDirection*speed*Time.fixedDeltaTime*Mathf.Cos(angle)*640;
-        float newY = VerticalDirection *speed *Time.fixedDeltaTime*Mathf.Sin(angle)*640;
+        float angle = Mathf.Atan2(VerticalDirection, HorizontalDirection);
+
+        //seting up the animator settings
+        animator.SetBool("isWalking", hypotenuse>0);
+        if(hypotenuse>0)
+            animator.SetFloat("angle", (angle*(180/Mathf.PI))+45);
+
+        float newX = HorizontalDirection*speed*Time.fixedDeltaTime*640;
+        float newY = VerticalDirection *speed *Time.fixedDeltaTime*640;
 
         //moving the player character around
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(newX, newY, 0);
     }
 
     private void StiffMovement () {
-        // will only have 4 directions of movement ==> Up, Down, Left, Right
+        //will only have 4 directions of movement ==> Up, Down, Left, Right
         //plyayer should move in the direction of the most recent input, regardless of other inputs
 
         //either 1 or 0 or -1 in a given direction
@@ -59,7 +61,8 @@ public class TestWalking : MonoBehaviour
         walkingHorizontally = HorizontalDirection != 0;
         walkingVertically = VerticalDirection != 0;
 
-        // seting up the animator settings
+        //seting up the animator settings
+        //will need to be updated if we ever switch back to this movement scheme
         animator.SetBool("isWalking",HorizontalDirection != 0 || VerticalDirection != 0); 
         animator.SetFloat("HorizontalSpeed", HorizontalDirection);
         animator.SetFloat("VerticalSpeed", VerticalDirection);
