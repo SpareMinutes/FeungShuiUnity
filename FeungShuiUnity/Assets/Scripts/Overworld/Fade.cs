9019a819122d0dbf0e4f590b88f1e9a9896ee255
MegaMinerd <megaminerd@gmail.com>
2019-07-04 18:10:38 -0400

    Added bridge to TestEnvironment

--- FeungShuiUnity/Assets/Scripts/Overworld/Fade.cs
+++ FeungShuiUnity/Assets/Scripts/Overworld/Fade.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Fade : MonoBehaviour {
     public GameObject sprite, player;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(player.transform.position.z > sprite.transform.position.z)
-            sprite.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+        if(player.transform.position.z > sprite.transform.position.z) {
+            sprite.GetComponent<Tilemap>().color = new Color(1f, 1f, 1f, 0.5f);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
-        sprite.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        sprite.GetComponent<Tilemap>().color = new Color(1f, 1f, 1f, 1f);
     }
 }
