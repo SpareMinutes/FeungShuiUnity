d508866f33a4fe96f26f2ad7897e74b90d3d9e0f
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-07-21 21:46:51 +1000

    just changed the ally move in moveMaster

--- FeungShuiUnity/Assets/Scripts/Data/MovesMaster.cs
+++ FeungShuiUnity/Assets/Scripts/Data/MovesMaster.cs
@@ -9,7 +9,7 @@ public class MovesMaster{
         {"Surf", new Move(90, 1, 1, "Water", 0.01f, false, Move.Target.Others, null)},
         {"Fissure", new Move(10^12, 0.3f, 1, "Earth", 0, false, Move.Target.Single, null)}, // 1,000,000,000,000 damage should be enough for a one hit (dont think we should have OHKO in our game)
         {"Howl", new Move(0, 1, 1, "Water", 0, false, Move.Target.Self, new Dictionary<string,List<string>>{{"Buff", new List<string>{"0", "0"}}})},
-        {"_Ally", new Move(10,1,1,"Beast",0f,false,Move.Target.Ally,new Dictionary<string,List<string>>{{"Buff",new List<string> {"0", "0"}}})}
+        {"Ally", new Move(10,1,1,"Beast",0f,false,Move.Target.Ally,new Dictionary<string,List<string>>{{"Buff",new List<string> {"0", "0"}}})}
     };
 
     public static Move Find(string name){
