9079a167767c252edaf1a7c8d20a19bdfbd66870
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-07-27 22:33:21 +1000

    run into a problem
    using GameObject.Find() doesnt return inactive GameObjects.
    this dosent work with what ive got for not selecting dead spirits, ill save what i have now and talk about it later

--- FeungShuiUnity/Assets/Scripts/Battle/CreatureBattleStatusController.cs
+++ FeungShuiUnity/Assets/Scripts/Battle/CreatureBattleStatusController.cs
@@ -19,14 +19,24 @@ public class CreatureBattleStatusController : MonoBehaviour {
         Stamina.transform.localScale = new Vector3(7.5f * (Target.currentActiveHealth / Target.maxActiveHealth), 1, 0);
         Critical.transform.localScale = new Vector3(4f * (Target.currentCriticalHealth / Target.maxCriticalHealth), 1, 0);
         if (Target.currentActiveHealth <= 0){
-            this.gameObject.SetActive(false);
+            //this.gameObject.SetActive(false);
             //remove Target from the Turn manager list
             ES.GetComponent<TurnManager>().removeFromPlay(Target);
+
+            //after its removed from play set the dead cover up for that spirit
+            for (int i=1; i <= 4; i++) {
+                GameObject spirit = GameObject.Find("Spirit" + i + "Status");
+                
+                if (spirit.GetComponent<CreatureBattleStatusController>().Target == this.Target) {
+                    GameObject.Find("Spirit" + i + "Cover(Dead)").GetComponent<Image>().enabled = true;
+                }
+            }
 
             //To do: ask for replacement
                 //would just reassign the Target
                 //ask player to choose from their party 
                 //enemy AI would just choose a random non fainted spirit from their party
+                //selecton would happen at the end of combat in the menu (todo)
         }
     }
 }
