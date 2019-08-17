39da13889eacb3a5f17b9d3a02273e08e394a56b
MegaMinerd <megaminerd@gmail.com>
2019-08-17 16:09:38 -0400

    Added bounds to camera movement

---
+++ FeungShuiUnity/Assets/Scripts/Overworld/PanToFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanToFollow : MonoBehaviour{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private Rect bounds;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        this.transform.position = new Vector3(Mathf.Max(Mathf.Min(target.transform.position.x, bounds.xMax), bounds.xMin), Mathf.Max(Mathf.Min(target.transform.position.y, bounds.yMax), bounds.yMin), -10);
+    }
+}
