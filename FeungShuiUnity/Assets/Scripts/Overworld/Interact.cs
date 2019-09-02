2445570a5967c2ce5679a94c57dc46bed13c09fd
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-09-02 20:09:26 +1000

    fixed the altar not working glitch

--- FeungShuiUnity/Assets/Scripts/Overworld/Interact.cs
+++ FeungShuiUnity/Assets/Scripts/Overworld/Interact.cs
@@ -14,19 +14,27 @@ public class Interact : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
+        //controls where the interaction area is compared to the player (NESW)
         area.transform.rotation = Quaternion.Euler(0, 0, animator.GetInteger("angle") + 45);
+
+        //press enter
         if (Input.GetButtonDown("Submit")){
+            Debug.Log("attempted to interact");
+
             List<Collider2D> lookedAt = new List<Collider2D>();
             ContactFilter2D filter = new ContactFilter2D();
             filter.useTriggers = true;
             area.GetComponent<PolygonCollider2D>().OverlapCollider(filter, lookedAt);
+            Debug.Log(lookedAt.Count);
             foreach(Collider2D coll in lookedAt){
+                Debug.Log(coll.gameObject.name);
                 EventTrigger e = coll.GetComponent<EventTrigger>();
-                if (e != null) {
+                if (e != null && coll.gameObject.tag.Equals("Interactable")) {
                     e.onInteract.Invoke();
                     break;
                 }
             }
+            Debug.Log("\n");
         }
 
     }
