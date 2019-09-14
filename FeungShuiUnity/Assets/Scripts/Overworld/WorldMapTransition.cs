1678c913c5c61d13c0e286c5c920deeef8e39789
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-09-14 22:30:37 +1000

    cleaned up the previous code

--- FeungShuiUnity/Assets/Scripts/Overworld/WorldMapTransition.cs
+++ FeungShuiUnity/Assets/Scripts/Overworld/WorldMapTransition.cs
@@ -15,18 +15,19 @@ public class WorldMapTransition : MonoBehaviour {
     public void TransitionHorizontal(bool eastward) {
         //keep the same y but the x position is the other side of the map
         PersistentStats.SceneChangePosY = gameObject.transform.position.y;
-        if (eastward) {
+        PersistentStats.SceneChangePosX = eastward ? (-(1600 - offset)) : (1600 - offset);
+        /*if (eastward) {
             PersistentStats.SceneChangePosX = -(1600 - offset);
         } else {
-            PersistentStats.SceneChangePosX = 1600  - offset;
-        }
+            PersistentStats.SceneChangePosX = 1600 - offset;
+        }*/
         PersistentStats.SceneChanged = true;
 
         int newCol = Col + (eastward ? 1 : -1);
         try {
             Debug.Log(MapGrid[Row].Maps[newCol]);
             SceneManager.LoadScene(MapGrid[Row].Maps[newCol]);
-            transform.position = new Vector3(0 - gameObject.transform.position.x, gameObject.transform.position.y, 0);
+            //transform.position = new Vector3(0 - gameObject.transform.position.x, gameObject.transform.position.y, 0);
         }catch (IndexOutOfRangeException e) {
 
         }catch(NullReferenceException e) {
@@ -37,18 +38,14 @@ public class WorldMapTransition : MonoBehaviour {
     public void TransitionVertical(bool southward) {
         //x stays the same and y inverts
         PersistentStats.SceneChangePosX = gameObject.transform.position.x;
-        if (southward) {
-            PersistentStats.SceneChangePosY = 1600 - offset;
-        } else {
-            PersistentStats.SceneChangePosY = -(1550 - offset);
-        }
+        PersistentStats.SceneChangePosX = southward ? (1600 - offset) : (-(1600 - offset));
         PersistentStats.SceneChanged = true;
 
         int newRow = Row + (southward ? 1 : -1);
         try {
             Debug.Log(MapGrid[Row].Maps[newRow]);
             SceneManager.LoadScene(MapGrid[newRow].Maps[Col]);
-            transform.position = new Vector3(gameObject.transform.position.x, 0 -gameObject.transform.position.y, 0);
+            //transform.position = new Vector3(gameObject.transform.position.x, 0 -gameObject.transform.position.y, 0);
         } catch (IndexOutOfRangeException e) {
 
         } catch (NullReferenceException e) {
