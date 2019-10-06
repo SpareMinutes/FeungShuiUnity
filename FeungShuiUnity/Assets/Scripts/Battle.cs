bbfffc23b47261b0aea5a8d92d1f64bd738df539
MegaMinerd <megaminerd@gmail.com>
2019-10-06 13:45:03 -0400

    Created NPC prefab

---
+++ FeungShuiUnity/Assets/Scripts/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour{
+    [SerializeField]
+    private Creature[] party;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
