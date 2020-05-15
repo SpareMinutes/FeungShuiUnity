b6de6b145c5db7f69de81125bd4a389445861391
notginmer <50023292+notginmer@users.noreply.github.com>
2020-05-14 19:51:48 -0700

    Updated damage formula
    updated damage formula, also fixed the greenclothes animation

--- FeungShuiUnity/Assets/Scripts/Data/Effect.cs
+++ FeungShuiUnity/Assets/Scripts/Data/Effect.cs
@@ -65,7 +65,7 @@ public class Effect {
         float relevantDefenseStat = target.getDefense(MakesContact);
 
         //calculate damage formula
-        float damageToTake = Mathf.Floor((power * relevantAttackStat*(3/2) * damageModifier) / (relevantDefenseStat));
+        float damageToTake = Mathf.Floor((power * Mathf.Pow(relevantAttackStat, 3/2f) * damageModifier) / (relevantDefenseStat * 15));
         user.ApplyDamage(damageToTake, target);
     }
 
