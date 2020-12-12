feeadd3b2414bd205b61fb2c72be354e23be5b0f
MegaMinerd <megaminerd@gmail.com>
2020-12-12 18:27:29 -0500

    Added eidola following

---
+++ FeungShuiUnity/Assets/Scripts/Overworld/SpiritFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritFollow : MonoBehaviour{
+    public GameObject player, other;
+    public float followDist, separateDist, speed;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector2 separation = player.transform.position - transform.position;
+        if(separation.magnitude < followDist) {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        } else {
+            separation.Normalize();
+            GetComponent<Rigidbody2D>().velocity = separation * speed;
+        }
+
+        separation = other.transform.position - transform.position;
+        if (separation.magnitude < separateDist) {
+            separation.Normalize();
+            GetComponent<Rigidbody2D>().velocity -= separation * (speed/4);
+        }
+    }
+}
