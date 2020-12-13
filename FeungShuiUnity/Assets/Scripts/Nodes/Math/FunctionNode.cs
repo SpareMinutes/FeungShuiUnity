e58762a50f8d1ab18b54fb864ee1a55fd8422de8
MegaMinerd <megaminerd@gmail.com>
2020-12-13 13:44:11 -0500

    Renamed and added to UnaryNode

---
+++ FeungShuiUnity/Assets/Scripts/Nodes/Math/FunctionNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("Math/Function"), NodeTint(100, 150, 180)]
+public class FunctionNode : ProcessorNode {
+    [Input(backingValue = ShowBackingValue.Unconnected)] public float value;
+    public Operation operation;
+    [Output] public float output;
+
+    public override object GetValue(GameObject context) {
+        NodePort firstPort = GetInputPort("value");
+        if (operation == Operation.AbsoluteValue) {
+            float val = firstPort.ConnectionCount == 0 ? value : float.Parse(((ProcessorNode)firstPort.GetConnection(0).node).GetValue(context).ToString());
+            return Mathf.Abs(val);
+        } else {
+            List<NodePort> ports = firstPort.GetConnections();
+            List<float> vals = new List<float>();
+            foreach(NodePort port in ports) {
+                vals.Add(float.Parse(((ProcessorNode)port.node).GetValue(context).ToString()));
+            }
+            switch (operation) {
+                case Operation.Min:
+                    return vals.Min();
+                case Operation.Max:
+                    return vals.Max();
+                case Operation.Sum:
+                    return vals.Sum();
+                default:
+                    return false;
+            }
+        }
+    }
+
+    public enum Operation {
+        AbsoluteValue,
+        Min,
+        Max,
+        Sum
+    }
+}
