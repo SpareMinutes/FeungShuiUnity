0d4f1f1143a168cc04c55d01364be7d1b7aaf669
MegaMinerd <megaminerd@gmail.com>
2021-02-07 23:11:30 -0500

    Implemented GenericMultistate

---
+++ FeungShuiUnity/Assets/Scripts/Nodes/Data/GetContextConfigNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("Data/GetContextConfig"), NodeTint(200, 150, 80)]
+public class GetContextConfigNode : ProcessorNode {
+    public string component, field;
+    [Output] public string output;
+
+    public override object GetValue(GameObject context) {
+        Component comp = context.GetComponent(System.Type.GetType(component));
+        return comp.GetType().GetField(field).GetValue(comp);
+    }
+}
---
+++ FeungShuiUnity/Assets/Scripts/Nodes/Data/SetContextConfigNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("Data/SetContextConfig"), NodeTint(200, 150, 80)]
+public class SetContextConfigNode : ExecutableNode {
+    public string component, field;
+    [Input(backingValue = ShowBackingValue.Unconnected)] public string value;
+    [Input(backingValue = ShowBackingValue.Never)] public bool previous;
+    [Output] public bool next;
+
+    public override void Execute(GameObject context) {
+        Component comp = context.GetComponent(System.Type.GetType(component));
+        string v = value.Equals("") ? ((ProcessorNode)GetInputPort("value").GetConnection(0).node).GetValue(context).ToString() : value;
+        comp.GetType().GetField(field).SetValue(comp, v);
+        ExecuteNext(GetOutputPort("next"), context);
+    }
+}
